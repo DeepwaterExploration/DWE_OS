a3976d82e848ed79061c3bd9f5e429be1d8a6414
brandonhs <brandonnms@gmail.com>
2022-02-03 11:57:51 -0800

    Fully Dynamic Devices

--- ui/index.js
+++ ui/index.js
@@ -7,6 +7,8 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.use('/', express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     res.render('index', {
@@ -16,7 +18,16 @@ app.get('/', function(req, res) {
             '/dev/video6'
         ]
     });
-})
+});
+
+// FIXME: This is a hacky approach
+app.get('/device', function(req, res) {
+    let device = req.query.device;
+    let index = req.query.index;
+    res.render('device', {
+        device, deviceIndex: index
+    });
+});
 
 app.listen(port, function() {
     console.log(`App listening on ${port}`);
--- ui/public/js/explorehd.js
+++ ui/public/js/explorehd.js
@@ -1,37 +1,72 @@
-// exploreHD setup
-var exploreHD = document.getElementsByClassName('explorehd-wrapper');
-
-for (let i = 0; i < exploreHD.length; i++) {
-    let ehd = exploreHD[i];
-    let slider = ehd.querySelector('#bitrate-slider');
-    let vbr = ehd.querySelector('#vbr-slider-' + i);
-    let bitrate = ehd.querySelector('#bitrate-text');
-    let h264Slider = ehd.querySelector('#gop-slider-' + i);
-    let h264Label = ehd.querySelector('#gop-slider-label');
-    vbr.onclick = function() {
-        slider.disabled = vbr.checked;
-        if (vbr.checked) {
-            h264Label.classList.remove('is-checked');
-            h264Slider.checked = false;
-        }
-        h264Slider.disabled = vbr.checked;
-        bitrate.style.color = vbr.checked ? '#0000005A' : '#0000008A';
-    }
+function stringToHTML(str) {
+    var dom = document.createElement('div');
+    dom.innerHTML = str;
+    return dom.firstChild;
 }
 
-// Dialog Setup
-var setDefault = document.getElementById('set-default');
-var dialog = document.getElementById('default-dialog');
+var deviceIndex = 0;
 
-setDefault.addEventListener('click', function() {
-    dialog.showModal();
-});
+function createDevice(device) {
+    let req = $.ajax({
+        url: '/device',
+        type: 'get', 
+        data: {
+            device, 
+            index: deviceIndex
+        }, 
+        success: function(res) {
+            var ehd = stringToHTML(res);
+            document.querySelector('#device-wrapper').appendChild(ehd);
+            componentHandler.upgradeDom();
 
-dialog.querySelector('.close').addEventListener('click', function() {
-    dialog.close();
-});
+            let slider = ehd.querySelector('#bitrate-slider');
+            let vbr = ehd.querySelector('#vbr-slider-' + deviceIndex);
+            let bitrate = ehd.querySelector('#bitrate-text');
+            let h264Slider = ehd.querySelector('#gop-slider-' + deviceIndex);
+            let h264Label = ehd.querySelector('#gop-slider-label');
+            vbr.onclick = function() {
+                slider.disabled = vbr.checked;
+                if (vbr.checked) {
+                    h264Label.classList.remove('is-checked');
+                    h264Slider.checked = false;
+                }
+                h264Slider.disabled = vbr.checked;
+                bitrate.style.color = vbr.checked ? '#0000005A' : '#0000008A';
+            }
+        }, 
+        error: function() {
+            console.error('Error getting /device');
+        }
+    });
+    deviceIndex++;
+    return req;
+}
 
-dialog.querySelector('.set').addEventListener('click', function() {
-    // TODO: Set the settings
-    dialog.close();
-});
+var devices = [
+    '/dev/video2',
+    '/dev/video4',
+    '/dev/video6',
+];
+let requests = [];
+for (let device of devices) {
+    requests.push(createDevice(device));
+}
+
+$.when(...requests).done(function() {
+    // Dialog Setup
+    var setDefault = document.getElementById('set-default');
+    var dialog = document.getElementById('default-dialog');
+
+    setDefault.addEventListener('click', function() {
+        dialog.showModal();
+    });
+
+    dialog.querySelector('.close').addEventListener('click', function() {
+        dialog.close();
+    });
+
+    dialog.querySelector('.set').addEventListener('click', function() {
+        // TODO: Set the settings
+        dialog.close();
+    });
+})
