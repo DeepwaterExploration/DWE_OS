059999da7d48085d4b1d7f14f2d3e760a01e1753
brandonhs <brandonnms@gmail.com>
2022-02-26 12:52:03 -0800

    Fix connection to pi

--- ui/index.js
+++ ui/index.js
@@ -89,6 +89,6 @@ app.get('*', (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
     console.log(`App listening on port: ${port}`);
 });
