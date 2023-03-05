ad1a7e37ee05b1a637b3eb98b0e3caae7275b449
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-03-04 23:32:44 -0800

    Added icons to menu

--- ui/client/src/components/Header.js
+++ ui/client/src/components/Header.js
@@ -1,53 +1,66 @@
-import React, { useState } from "react";
+import * as React from 'react'
 
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/icons-material/Menu';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Box from "@mui/system/Box";
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew'
+import Divider from '@mui/material/Divider'
+import Drawer from '@mui/material/Drawer'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import Box from '@mui/system/Box'
 
-import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
-
-import DWELogo_white from '../images/DWELogo_white.svg';
-import { Grid } from "@mui/material";
-import WifiMenu from "./WifiMenu";
+import DWELogo_white from '../images/DWELogo_white.svg'
+import { Grid } from '@mui/material'
+import WifiMenu from './WifiMenu'
 
 export default function Header(props) {
-    const [drawer, setDrawer] = useState(false);
+  const [open, setOpen] = React.useState(false)
+  const toggleDrawer = () => {
+    setOpen(!open)
+  }
 
-    return (
-        <AppBar position="fixed" style={{ overflow: 'hidden' }}>
-            <Toolbar>
-                <IconButton onClick={() => setDrawer(!drawer) } edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                    <Menu />
-                </IconButton>
-                <Drawer anchor="left" open={drawer} onClose={ () => setDrawer(!drawer) }>
-                    <Box
-                        sx={{ width: 300 }}
-                        role="presentation"
-                    >
-                        <List>
-                            <ListItem>
-                                <Typography variant="h6" color="inherit">
-                                    DWE OS Pre-Alpha
-                                </Typography>
-                            </ListItem>
-                            <Divider />
-                            { props.drawerItems }
-                        </List>
-                    </Box>
-                </Drawer>
-                <img src={DWELogo_white} style={{ height: 30 }} alt="DWE Logo"/>
-                <Grid container justifyContent="flex-end">
-                    <WifiMenu />
-                </Grid>
-                { props.children }
-            </Toolbar>
-        </AppBar>
-    )
+  return (
+    <AppBar position="fixed" style={{ overflow: 'hidden' }}>
+      <Toolbar
+        sx={{
+          pr: '24px', // keep right padding when drawer closed
+        }}
+      >
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={toggleDrawer}
+          sx={{
+            marginRight: '36px',
+            ...(open && { display: 'none' }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer anchor="left" open={open} onClose={toggleDrawer}>
+          <Box sx={{ width: 300 }} role="presentation">
+            <List>
+              <ListItem>
+                <Typography variant="h6" color="inherit">
+                  DWE OS Pre-Alpha
+                </Typography>
+              </ListItem>
+              <Divider />
+              {props.drawerItems}
+            </List>
+          </Box>
+        </Drawer>
+        <img src={DWELogo_white} style={{ height: 30 }} alt="DWE Logo" />
+        <Grid container justifyContent="flex-end">
+          <WifiMenu />
+        </Grid>
+        <PowerSettingsNewIcon />
+        {props.children}
+      </Toolbar>
+    </AppBar>
+  )
 }
